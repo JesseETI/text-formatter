a5a0b103b45dcdb072d475437c52079486afa066
Jesse Emamalie <jesseemamalie@Jesses-MacBook-Pro.local>
2021-11-25 14:27:28 -0400

    Revised the project to demonstrate my python improvement

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
-This project was made using python3 as a quick way to format text.
+# Python Text Formatter
 
-Options:
-1. Uppercase Text
-2. Lowercase Text
-3. Format text as a sentence
-4. Import text from a file
-5. Export text to a file
-6. Exit
+Takes raw text via keyboard or text file and formats it. Export to file option available.
+
+## Getting Started
+
+Clone / Download the repository to your PC. Ensure python3 is installed, then cd into local repo directory.
+python3 text_formatter.py
+
+You can freely edit the input text file however you'd like for your benefit
+
+### Prerequisites
+1. Python3 (Built & tested with Python 3.9.7)
+
+## Deployment
+
+
+## Built With
+
+* Python3 as a terminal script
+
+Demonstrates:
+    - Recursion
+    - Object Oriented Programming / Class Paradigm
+    - Built-in Python imports
+    - Handling of input exceptions
+    - String modification & REGEX
+
+## Authors
+
+* **ETI Designs** - http://www.etidesigns.com/
--- text_formatter.py
+++ text_formatter.py
@@ -1,189 +1,167 @@
-import os #used for clearing cmd screen
+import os
 import re #used to split sentences based on multiple punctuation marks (RegEx)
 import sys #used for inputting multiple lines as a string
 import subprocess #used to open file in text editor on mac
+from signal import signal, SIGINT #to test if SIGINT is called (Cmd + C)
 
-#global variables
-str = ""
-edit_text = ""
-
-#method for checking if user wants to repeat process
-def repeat_method(methodID):
-    choice = input("Enter Y to repeat Method, or N to return to Main Menu: ")
-
-    if choice == "Y": #gets methodID from being called in method
-
-        if methodID == 1:
-            uppercase()
-
-        elif methodID == 2:
-            lowercase()
-        
-        elif methodID == 3:
-            sentence()
-        
-        elif methodID == 4:
-            import_text()
-
-        elif methodID == 5:
-            export_text()
-        
+class TextManager:
+    def __init__(self):
+        self.rawText = None
+        self.formattedText = None   
+    
+    def formatUpperCase(self):
+        # for mac / linux
+        if os.name == 'posix':
+            os.system('clear')
         else:
-            print("Something went wrong...Exiting\n")
-            exit()
-                
-    elif choice == "N": #redirects user to menu if they want to go.
-        menu()
+            # for windows platfrom
+            os.system('cls')
     
-    else:
-        input("Choice was invalid. Please try again..")
-        repeat_method(methodID) #makes user repeat method if input is invalid
+        print("*********     UPPERCASE CONVERTER      ********* \n")
 
-#function checks if string is imported and if it isn't, it reads user input
-def isImported():
-    global str
-
-    if not str == "":
-        print("Text has been imported. Raw Text: \n")
-        print(str)
-    else:
-        print("Press control + D twice when you have finished entering your text.\n \nRaw Text: \n")
-        str = sys.stdin.read()
-
-def uppercase():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    methodID = 1 #method IDs passed for repeat method to identify method that needs to be repeated
-    global str
-    global edit_text
-    
-    print("*********     UPPERCASE CONVERTER      ********* \n")
-    
-    isImported()
-    
-    edit_text = str.upper()
-    
-    print("\n \nThis is your edited text: \n \n" + edit_text)
-
-    repeat_method(methodID)
-
-def lowercase():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    methodID = 2
-    global str
-    global edit_text
-
-    print("*********     LOWERCASE CONVERTER      ********* \n")
-
-    isImported()
-
-    edit_text = str.lower()
-    
-    print("\n \nThis is your edited text: \n \n" + edit_text)
-
-    repeat_method(methodID)
-
-def sentence():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    methodID = 3
-    global str
-    global edit_text
-    
-    end_punc = [".", "?", "!"] #list of punctuations to check if string ends with them
-    
-    print("*********     SENTENCE FORMATTER      ********* \n")
-    print("This does not automatically include punctuation marks except the period. \n \n"\
-          "If there are multiple sentences, please separate them by punctuation marks. \n")
-
-    isImported()
-
-    if not str.endswith(tuple(end_punc)) and not str == "":
-        str = str + "." #converts list to tuple for endswith() method to function
-
-    edit_text = "".join(i[:1].upper() + i[1:] for i in re.split(r"(\s*[\.\?!]\s*) *", str)) #correct regex
-
-    '''
-    FOR THIS REGEX:
-        r - interprets the special characters as they are and not literal
-        () - to check regex in order exact
-        \s* - 0 or more whitespace to consider if spacing is there
-        [] - to check if ANY of the characters inside are match
-        \. - escape character for dot
-        \? - escape character for question mark
-        * - checks if any of the punctuation marks were entered after each other eg. ?! or ..
+        if self.rawText:
+            print("This is your raw text: ", self.rawText)
+        else:
+            self.rawText = input("Please enter/paste the text you'd like to make uppercase: ")
         
-    FOR .join():
-        capitalizes first letter in first word +  rest of sentence that is split
-    '''
+        self.formattedText = self.rawText.upper()
+        print("\nThis is your edited text: " + self.formattedText)
+        self.exportOrExit()
 
-    print("\n \nThis is your edited text: \n \n" + edit_text)
+    def formatLowerCase(self):
+        # for mac / linux
+        if os.name == 'posix':
+            os.system('clear')
+        else:
+            # for windows platfrom
+            os.system('cls')
 
-    repeat_method(methodID)
+        print("*********     LOWERCASE CONVERTER      ********* \n")
+        
+        if self.rawText:
+            print("This is your raw text: ", self.rawText)
+        else:
+            self.rawText = input("Please enter/paste the text you'd like to make lowercase: ")
+        
+        self.formattedText = self.rawText.lower()
+        print("\nThis is your edited text: " + self.formattedText)
+        self.exportOrExit()
 
-def import_text():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    methodID = 4
-    global str
+    def formatSentence(self):
+        # for mac / linux
+        if os.name == 'posix':
+            os.system('clear')
+        else:
+            # for windows platfrom
+            os.system('cls')
 
-    print("*********     IMPORT TEXT      ********* \n")
-    print("This allows you to import your unformatted text from a text file for formatting. \n")
-    print("Store this file in the same directory as this program. \n")
-
-    file_name = input("Enter file name in the format file_name.txt: ")
+        end_punc = [".", "?", "!"] #list of punctuations to check if string ends with them
     
-    #checks if file is formatted correctly with .txt
-    if file_name.endswith(".txt"):
-        file = open(file_name, "r")
+        print("""*********     SENTENCE / PARAGRAPH FORMATTER      *********\n
+        \nThis will add capitalize each sentence and add a period if the last sentence doesn't finish with a punctuation.\n""")
+        
+        if self.rawText:
+            print("This is your raw text: ", self.rawText)
+        else:
+            self.rawText = input("Please enter/paste the text you'd like to format: ")
+
+        str = self.rawText 
+
+        if not str.endswith(tuple(end_punc)) and not str == "":
+            str = str + "." #add period to end of sentence / paragraph
+
+        self.formattedText = "".join(i[:1].upper() + i[1:] for i in re.split(r"(\s*[\.\?\!]\s*) *", str)) #correct regex
+
+        '''
+        FOR THIS REGEX:
+            r - interprets the special characters as they are and not literal
+            () - to check regex in order exact
+            \s* - 0 or more whitespace to consider if spacing is there
+            [] - to check if ANY of the characters inside are match
+            \. - escape character for dot
+            \? - escape character for question mark
+            * - checks if any of the punctuation marks were entered after each other eg. ?! or ..
+            
+        FOR .join():
+            capitalizes first letter in first word +  rest of sentence that is split
+        '''
+
+        print("\nThis is your edited text: " + self.formattedText)
+        self.exportOrExit()
+
+    def importText(self):
+        # for mac / linux
+        if os.name == 'posix':
+            os.system('clear')
+        else:
+            # for windows platfrom
+            os.system('cls')
+
+        print("""*********     IMPORT TEXT      *********\n
+        \nThis allows you to import your unformatted text from a text file for formatting. Place RAW text in sampleInput.txt in the same directory as this program.\n""")
+        
+        file = open("sampleInput.txt", "r")
         str = file.read()
-    else:
-        input("File name not valid. Press Enter to try again...")
-        import_text()
-    
-    print("\nThis is your imported unformatted text: \n")
-    print(str)
-
-    repeat_method(methodID)
-
-
-
-def export_text():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    methodID = 5
-    global edit_text
-    
-    print("*********     EXPORT TEXT      ********* \n")
-    print("This allows you to export your formatted text to a text file. \n")
-
-    file_name = input("Enter file name you'd like to save in the format file_name.txt: ")
-    
-    if file_name.endswith(".txt"):
-        file = open(file_name, "w+")
-        file.write(edit_text)
-        file.close()
         
-        #checks OS to open file editor
-        if sys.platform == "win32":
-            os.startfile(file_name)
-    
+        input("\nThis is your imported text, stored for the running duration of this program: " + str)
+        self.rawText = str
+        menu(self)
+
+    def exportText(self):
+        # for mac / linux
+        if os.name == 'posix':
+            os.system('clear')
         else:
-            subprocess.call(["open",file_name])
-    
+            # for windows platfrom
+            os.system('cls')
+
+        print("""*********     EXPORT TEXT      *********\n
+        \nThis allows you to export your formatted text to a text file. \n""")
+
+        if not self.formattedText:
+            input("No text was formatted by program. Try doing so before exporting")
+            menu()
+
+        file_name = input("Enter file name you'd like to save in the format file_name.txt: ")
+        
+        if file_name.endswith(".txt"):
+            file = open(file_name, "w+")
+            file.write(self.formattedText)
+            file.close()
+            
+            #checks OS to open file editor
+            if sys.platform == "win32":
+                os.startfile(file_name)
+            else:
+                subprocess.call(["open",file_name])
+        
+        else:
+            input("File name not valid. Press Enter to try again...")
+            self.exportText()
+        
+        input("\nThe file has been written with formatted text. Check directory for txt file.")
+        menu()
+
+    #method for checking if user wants to export 
+    def exportOrExit(self):
+        choice = input("\nWould you like to EXPORT formatted text into a .txt file before returning to main menu(Y/N)?: ").upper()
+
+        if choice == "Y": #gets methodID from being called in method
+            self.exportText()
+        elif choice == "N": #redirects user to menu if they want to go.
+            menu()
+        else:
+            input("\nChoice was invalid. Press key to make choice again..\n")
+            self.exportOrExit() #makes user repeat method if input is invalid
+
+def menu(textManager = None):
+    # for mac / linux
+    if os.name == 'posix':
+        os.system('clear')
     else:
-        input("File name not valid. Press Enter to try again...")
-        export_text()
-    
-    print("\nThe file has been written with formatted text. Check directory for txt file.")
-
-    repeat_method(methodID)
-
-def menu():
-    os.system('cls')  # For Windows
-    os.system('clear')  # For Linux/OS X
-    
+      # for windows platfrom
+      os.system('cls')
+      
     print("*********     WELCOME TO THE TEXT FORMATTER      ********* \n")
     print("This program was created by JesseETI :) \n \n")
     
@@ -204,23 +182,26 @@ def menu():
     else:
         option = int(option)
         if option >= 1 and option <= 6:
+            if not textManager:
+                textManager = TextManager()
+            
             if option == 1:
-                uppercase()
+                textManager.formatUpperCase()
 
             elif option == 2:
-                lowercase()
+                textManager.formatLowerCase()
     
             elif option == 3:
-                sentence()
+                textManager.formatSentence()
 
             elif option == 4:
-                import_text()
+                textManager.importText()
     
             elif option == 5:
-                export_text()
+                textManager.exportText()
 
             elif option == 6:
-                exit()
+                exit(0)
     
             else:
                 print("Program has crashed unexpectedly. Exiting.. \n")
@@ -229,5 +210,11 @@ def menu():
             input("Option needs to be within 1 to 6. Press Enter to try again...")
             menu()
 
-#from menu choice, function is called. Therefore, only menu is called in program code. Rest is user input.
-menu()
+def handler(signal_received, frame):
+    # Handle any cleanup here
+    print('\nSIGINT or CTRL-C detected. Exiting gracefully')
+    exit(0)
+
+if __name__ == "__main__":
+    signal(SIGINT, handler)
+    menu()
